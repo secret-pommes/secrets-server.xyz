b2fab852bbaa744a7aec6cdf7cf34e3d39224bb6
Secret1337 <97087168+secret-pommes@users.noreply.github.com>
2023-06-17 11:32:37 +0200

    stuff

--- struct/version.js
+++ struct/version.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
+const version = require("../package.json").version;
+
 app.get("/api/versionchecker/latest", (req, res) => {
-  const version = require("../package.json").version;
   res.json([`Version: ${version}`]);
+});
+
+app.get("/api/versionchecker/raw", (req, res) => {
+  res.json([version]);
 });
 
 module.exports = app;
